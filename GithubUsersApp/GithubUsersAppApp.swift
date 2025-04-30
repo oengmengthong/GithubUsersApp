23ccb655650675afaf3b3f678fcecf6bfda86823
Oeng Mengthong <mengthongoenggl@gmail.com>
2025-04-30 16:17:36 +0700

    Initial Commit

---
+++ GithubUsersApp/GithubUsersAppApp.swift
@@ -0,0 +1,32 @@
+//
+//  GithubUsersAppApp.swift
+//  GithubUsersApp
+//
+//  Created by Mengthong on 30/4/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct GithubUsersAppApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ GithubUsersAppTests/GithubUsersAppTests.swift
@@ -0,0 +1,16 @@
+//
+//  GithubUsersAppTests.swift
+//  GithubUsersAppTests
+//
+//  Created by Mengthong on 30/4/25.
+//
+
+import Testing
+
+struct GithubUsersAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
